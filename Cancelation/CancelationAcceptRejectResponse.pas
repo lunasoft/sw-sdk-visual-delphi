45def3aa9d89f1405f9c2a32b15f0c44be67c7d9
Emmanuel <emmanuel.contreras@sw.com.mx>
2018-10-02 15:49:24 -0500

    Refactorización de la librería, se anexan nuevos metodos de cancelación, emisión-timbrado por JSON y por XML. Generación de clases para las respuestas, ya se manejan objetos(Serialización de JSON).

---
+++ Cancelation/CancelationAcceptRejectResponse.pas
@@ -0,0 +1,114 @@
+unit CancelationAcceptRejectResponse;
+
+interface
+
+uses Generics.Collections, Rest.Json;
+
+type
+
+TFoliosClass = class
+private
+  FEstatusUUID: String;
+  FRespuesta: String;
+  FUuid: String;
+public
+  property estatusUUID: String read FEstatusUUID write FEstatusUUID;
+  property respuesta: String read FRespuesta write FRespuesta;
+  property uuid: String read FUuid write FUuid;
+  function ToJsonString: string;
+  class function FromJsonString(AJsonString: string): TFoliosClass;
+end;
+
+TDataClass = class
+private
+  FAcuse: String;
+  FFolios: TArray<TFoliosClass>;
+public
+  property acuse: String read FAcuse write FAcuse;
+  property folios: TArray<TFoliosClass> read FFolios write FFolios;
+  destructor Destroy; override;
+  function ToJsonString: string;
+  class function FromJsonString(AJsonString: string): TDataClass;
+end;
+
+TCancelationAcceptRejectResponse = class
+private
+  FCodStatus: String;
+  FData: TDataClass;
+  FMessage: String;
+  FMessageDetail: String;
+public
+  property codStatus: String read FCodStatus write FCodStatus;
+  property data: TDataClass read FData write FData;
+  property message: String read FMessage write FMessage;
+  property messageDetail: String read FMessageDetail write FMessageDetail;
+  constructor Create;
+  destructor Destroy; override;
+  function ToJsonString: string;
+  class function FromJsonString(AJsonString: string): TCancelationAcceptRejectResponse;
+end;
+
+implementation
+
+{TFoliosClass}
+
+
+function TFoliosClass.ToJsonString: string;
+begin
+  result := TJson.ObjectToJsonString(self);
+end;
+
+class function TFoliosClass.FromJsonString(AJsonString: string): TFoliosClass;
+begin
+  result := TJson.JsonToObject<TFoliosClass>(AJsonString)
+end;
+
+{TDataClass}
+
+destructor TDataClass.Destroy;
+var
+  LfoliosItem: TFoliosClass;
+begin
+
+ for LfoliosItem in FFolios do
+   LfoliosItem.free;
+
+  inherited;
+end;
+
+function TDataClass.ToJsonString: string;
+begin
+  result := TJson.ObjectToJsonString(self);
+end;
+
+class function TDataClass.FromJsonString(AJsonString: string): TDataClass;
+begin
+  result := TJson.JsonToObject<TDataClass>(AJsonString)
+end;
+
+{TCancelationAcceptRejectResponse}
+
+constructor TCancelationAcceptRejectResponse.Create;
+begin
+  inherited;
+  FData := TDataClass.Create();
+end;
+
+destructor TCancelationAcceptRejectResponse.Destroy;
+begin
+  FData.free;
+  inherited;
+end;
+
+function TCancelationAcceptRejectResponse.ToJsonString: string;
+begin
+  result := TJson.ObjectToJsonString(self);
+end;
+
+class function TCancelationAcceptRejectResponse.FromJsonString(AJsonString: string): TCancelationAcceptRejectResponse;
+begin
+  result := TJson.JsonToObject<TCancelationAcceptRejectResponse>(AJsonString)
+end;
+
+end.
+
---
+++ unitTestDelphiRest.dpr
@@ -0,0 +1,39 @@
+program unitTestDelphiRest;
+
+uses
+  Vcl.Forms,
+  TestServices in 'TestServices.pas' {Form1},
+  SWSDK in 'SWSDK.pas',
+  Cancelation in 'Cancelation\Cancelation.pas',
+  Helper in 'Helpers\Helper.pas',
+  Stamp in 'Stamp\Stamp.pas',
+  Authentication in 'Authentication\Authentication.pas',
+  AuthenticationRequest in 'Authentication\AuthenticationRequest.pas',
+  AuthenticationResponse in 'Authentication\AuthenticationResponse.pas',
+  StampResponseV1 in 'Stamp\Response\StampResponseV1.pas',
+  StampResponseV2 in 'Stamp\Response\StampResponseV2.pas',
+  StampResponseV3 in 'Stamp\Response\StampResponseV3.pas',
+  StampResponseV4 in 'Stamp\Response\StampResponseV4.pas',
+  StampRequest in 'Stamp\StampRequest.pas',
+  CancelationResponse in 'Cancelation\CancelationResponse.pas',
+  CancelationRequest in 'Cancelation\CancelationRequest.pas',
+  Issue in 'Issue\Issue.pas',
+  Validation in 'Validate\Validation.pas',
+  ValidateLcoResponse in 'Validate\ValidateLcoResponse.pas',
+  ValidateLrfcResponse in 'Validate\ValidateLrfcResponse.pas',
+  ValidateCfdiResponse in 'Validate\ValidateCfdiResponse.pas',
+  ValidateRequest in 'Validate\ValidateRequest.pas',
+  JsonIssue in 'Issue\JsonIssue\JsonIssue.pas',
+  IssueJsonRequest in 'Issue\JsonIssue\IssueJsonRequest.pas',
+  CancelationRelationsResponse in 'Cancelation\CancelationRelationsResponse.pas',
+  CancelationPendingsResponse in 'Cancelation\CancelationPendingsResponse.pas',
+  CancelationAcceptRejectResponse in 'Cancelation\CancelationAcceptRejectResponse.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
